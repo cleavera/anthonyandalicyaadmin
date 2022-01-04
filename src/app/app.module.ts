272efc39a31b980011020a6af22c557bad4fa8dc
Anthony Cleaver <anthony.cleaver@mhr.co.uk>
2022-01-04 15:17:52 +0000

    Add guest service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { LineComponent } from './components/line/line.component';
 import { PageComponent } from './components/page/page.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { API_PROVIDER } from './providers/api/api.provider';
+import { GuestService } from './services/guest.service';
 import { InviteService } from './services/invite.service';
 
 @NgModule({
@@ -26,6 +27,7 @@ import { InviteService } from './services/invite.service';
     ],
     providers: [
         API_PROVIDER,
+        GuestService,
         InviteService
     ],
     bootstrap: [AppComponent]
---
+++ src/app/services/guest.service.ts
@@ -0,0 +1,45 @@
+import { Inject, Injectable } from '@angular/core';
+import { Api } from '@skimp/client';
+import { ResourceLocation } from '@skimp/core';
+import { GuestSchema } from 'anthony-and-alicya-domain';
+import { Observable, Subject } from 'rxjs';
+
+import { RepositoryInterface } from '../interfaces/repository.interface';
+import { API_TOKEN } from '../providers/api/api.token';
+
+@Injectable()
+export class GuestService implements RepositoryInterface<GuestSchema> {
+    private _guestSubjects: Record<string, Subject<GuestSchema>>;
+    private _listSubject: Subject<Array<GuestSchema>>;
+    private _api: Api;
+
+    constructor(@Inject(API_TOKEN) api: Api) {
+        this._api = api;
+        this._guestSubjects = {};
+        this._listSubject = new Subject<Array<GuestSchema>>();
+    }
+
+    public getAll(): Observable<Array<GuestSchema>> {
+        return this._listSubject.asObservable();
+    }
+
+    public get(location: ResourceLocation): Observable<GuestSchema> {
+        return this._get(location).asObservable();
+    }
+
+    public async loadAll(): Promise<void> {
+        this._listSubject.next(await this._api.list(GuestSchema));
+    }
+
+    public async load(location: ResourceLocation): Promise<void> {
+        this._get(location).next(await this._api.get(GuestSchema, location));
+    }
+
+    private _get(location: ResourceLocation): Subject<GuestSchema> {
+        if (this._guestSubjects[location.toString()]) {
+            this._guestSubjects[location.toString()] = new Subject<GuestSchema>();
+        }
+
+        return this._guestSubjects[location.toString()];
+    }
+}
